9b2f70b7e3ec38283ab6e76f6f9920bcce365d51
Felipe Sasdelli <sasdelli.pi@gmail.com>
2023-10-29 11:13:53 -0300

    Organizing init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,34 +1,40 @@
-local lspconfig = require'lspconfig'
-local configs = require'lspconfig.configs'
-
--- Install packer if not installed
+--------------------------------------------------------------------------------
+-- Installing Packer
+--------------------------------------------------------------------------------
+-- Install packer if not installed.
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  fn.system({ 'git'
+            , 'clone'
+            , 'https://github.com/wbthomason/packer.nvim'
+            , install_path
+            }
+           )
   vim.cmd 'packadd packer.nvim'
 end
 
--- Use packer to manage plugins
+--------------------------------------------------------------------------------
+-- Plugin Management
+--------------------------------------------------------------------------------
+-- Using Packer to manage plugins.
 require('packer').startup(function()
-  -- Packer can manage itself
+  -- Packer can manage itself.
   use 'wbthomason/packer.nvim'
-  -- Tokyo Night theme
+  -- Tokyo Night theme.
   use 'folke/tokyonight.nvim'
-  -- Status line plugin
+  -- Status line plugin.
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
-  -- Aiken Programming Language Support
-  use 'aiken-lang/editor-integration-nvim'
-  -- LSP
+  -- LSP.
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     requires = {
-      -- LSP Support
+      -- LSP Support.
       {'neovim/nvim-lspconfig'},
       {
         'williamboman/mason.nvim',
@@ -38,69 +44,65 @@ require('packer').startup(function()
       },
       {'williamboman/mason-lspconfig.nvim'},
 
-      -- Autocompletion
+      -- Autocompletion.
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-nvim-lsp'},
       {'L3MON4D3/LuaSnip'},
     }
   }
-  -- Tree sitter
+  -- Tree sitter.
   use 'nvim-treesitter/nvim-treesitter'
-  use 'tree-sitter/tree-sitter-scala'
-  -- Neovim comments
+  -- Neovim comments.
   use 'terrortylor/nvim-comment'
-  -- Indentation guide
+  -- Transparent.
+  use 'xiyaowong/transparent.nvim'
+  -- Indentation guide.
   use {
     'lukas-reineke/indent-blankline.nvim',
     commit = '9637670896b68805430e2f72cf5d16be5b97a22a'
   }
-  -- Telescope
+  -- Telescope.
   use {
     'nvim-telescope/telescope.nvim',
     requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' }
   }
-  -- Nvim Tree
+  -- Nvim Tree.
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {
-      'nvim-tree/nvim-web-devicons', -- optional
+      'nvim-tree/nvim-web-devicons',
     },
     config = function()
       require("nvim-tree").setup {}
     end
   }
-  -- Auto trim trailing whitespaces and lines
+  -- Auto trim trailing whitespaces and lines.
   use "cappyzawa/trim.nvim"
-  -- GitSigns
+  -- GitSigns.
   use 'lewis6991/gitsigns.nvim'
-  -- Auto pairs
+  -- Auto pairs.
   use 'windwp/nvim-autopairs'
-  -- Direnv
+  -- Direnv.
   use 'direnv/direnv.vim'
+  -- Aiken Programming Language Support.
+  use 'aiken-lang/editor-integration-nvim'
  end)
 
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- Set theme to Tokyo Night
+--------------------------------------------------------------------------------
+-- Neovim Settings
+--------------------------------------------------------------------------------
+-- Set theme to Tokyo Night.
 vim.cmd [[
   syntax enable
   colorscheme tokyonight
 ]]
 
--- Background color
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+-- Disable netrw at the very start of your init.lua (strongly advised).
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-vim.cmd [[
-  hi NvimTreeNormal guibg=NONE ctermbg=NONE
-  hi NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
-]]
-
-vim.cmd('highlight SignColumn guibg=NONE ctermbg=NONE')
+-- Set termguicolors to enable highlight groups.
+vim.opt.termguicolors = true
 
 -- Enable relative line numbers
 vim.wo.number = true
@@ -110,16 +112,16 @@ vim.wo.relativenumber = true
 vim.o.clipboard = "unnamedplus"
 
 -- 101 characters per line limit
-vim.wo.colorcolumn = "101"
+vim.wo.colorcolumn = "81"
 
 -- Set scroll off to 5 lines
-vim.o.scrolloff = 5
+-- vim.o.scrolloff = 5
 
 -- Set default indentation
-vim.opt.tabstop = 2     -- Set the number of spaces a tab in the file counts for
-vim.opt.softtabstop = 2 -- Set the number of spaces a <Tab> in insert mode counts for
-vim.opt.shiftwidth = 2  -- Set the number of spaces for autoindenting
-vim.opt.expandtab = true-- Converts tabs to spaces
+vim.opt.tabstop = 2     -- Set the number of spaces for <Tab> in the file.
+vim.opt.softtabstop = 2 -- Set the number of spaces for a <Tab> in insert mode.
+vim.opt.shiftwidth = 2  -- Set the number of spaces for autoindenting.
+vim.opt.expandtab = true-- Converts tabs to spaces.
 
 -- List of languages with 4 spaces indentation
 local four_spaces_languages = {"python", "c", "cpp", "haskell", "elm"}
@@ -129,16 +131,18 @@ for _, lang in ipairs(four_spaces_languages) do
   vim.cmd(string.format("autocmd FileType %s setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab", lang))
 end
 
-require('gitsigns').setup()
-
-require('nvim_comment').setup()
-
 vim.api.nvim_exec([[
   autocmd FileType aiken setlocal commentstring=//%s
   autocmd FileType nix setlocal commentstring=#%s
 ]], false)
 
--- LSP configuration
+--------------------------------------------------------------------------------
+-- LSP Configuration
+--------------------------------------------------------------------------------
+local lspconfig = require'lspconfig'
+local configs = require'lspconfig.configs'
+
+-- LSP configuration.
 require("mason").setup()
 local lsp = require('lsp-zero').preset({})
 
@@ -147,17 +151,6 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 lsp.setup()
-
--- Rust LSP
-require'lspconfig'.rust_analyzer.setup{
-  cmd = { "/run/current-system/sw/bin/rust-analyzer" }
-}
-
--- Haskell LSP
-require'lspconfig'.hls.setup{}
-
--- Scala LSP
-require'lspconfig'.metals.setup{}
 
 -- Lua LSP
 require'lspconfig'.lua_ls.setup {
@@ -179,6 +172,13 @@ require'lspconfig'.lua_ls.setup {
     }
   }
 }
+
+-- OCaml LSP
+require'lspconfig'.ocamllsp.setup{
+}
+
+-- Haskell LSP
+require'lspconfig'.hls.setup{}
 
 -- Aiken LSP
 if not configs.aiken then
@@ -203,7 +203,31 @@ require'lspconfig'.aiken.setup{
   end,
 }
 
--- Autocompletion
+--------------------------------------------------------------------------------
+-- Plugin Configuration
+--------------------------------------------------------------------------------
+require('gitsigns').setup()
+
+require('nvim_comment').setup()
+
+require("transparent").setup({
+  groups = {
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+  },
+  extra_groups = { 'NormalFloat',
+                   'FloatBorder',
+                   'NvimTreeWinSeparator',
+                   'NvimTreeNormal',
+                   'NvimTreeNormalNC',
+                   'TelescopeNormal',
+                   'TelescopeBorder',
+                 },
+  exclude_groups = {},
+})
+
 local cmp = require'cmp'
 
 cmp.setup({
@@ -213,7 +237,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),  -- This line is important
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<Tab>'] = function(fallback)
       if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
@@ -234,7 +258,6 @@ cmp.setup({
     end,
   },
 
-  -- You should specify your *installed* sources.
   sources = {
     { name = 'nvim_lsp' },
     { name = 'buffer' },
@@ -242,35 +265,23 @@ cmp.setup({
 })
 
 require('nvim-treesitter.configs').setup {
-  -- Install these parsers
-  ensure_installed = {"nix", "lua", "haskell", "rust", "typescript"},
+  ensure_installed = {"nix", "lua", "ocaml", "haskell", "typescript"},
   highlight = {
     enable = true,
-    disable = {"scala"},
+    disable = {},
   },
 }
 
 require("indent_blankline").setup {
-  -- The character used for indentation guides
   char = "│",
-  -- Exclude indentation guides in terminal buffers
   buftype_exclude = { "terminal" },
-  -- Disable indentation guides for blank lines
   show_trailing_blankline_indent = false,
-  -- Disable indentation guide for the first indent level
   show_first_indent_level = false,
-  -- Use treesitter to determine indentation level
   use_treesitter = true,
 }
 
 require('trim').setup({
-  -- Ignore markdown files
   ft_blocklist = {"markdown"},
-
-  -- Replace multiple blank lines with a single line
-  -- patterns = {
-  --   [[%s/\(\n\n\)\n\+/\1/]],
-  -- },
 })
 
 require("nvim-autopairs").setup ({
@@ -285,7 +296,7 @@ require("nvim-autopairs").setup ({
     map = "<M-e>",
     chars = { "{", "[", "(", '"', "'" },
     pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-    offset = 0, -- Offset from pattern match
+    offset = 0,
     end_key = "$",
     keys = "qwertyuiopzxcvbnmasdfghjkl",
     check_comma = true,
@@ -338,34 +349,34 @@ require('lualine').setup {
   }
 }
 
--- Shorten function name
+--------------------------------------------------------------------------------
+-- Keybindings
+--------------------------------------------------------------------------------
+-- Shorten function name.
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Scroll up/down while maintaing the cursor in the middle of the screen
+-- Scroll up/down while maintaing the cursor in the middle of the screen.
 keymap('n', '<C-d>', '<C-d>zz', opts)
 keymap('n', '<C-u>', '<C-u>zz', opts)
 
--- Key mapping for opening Telescope
--- keymap('n', '<Space>sf', '<Cmd>Telescope<CR>', opts)
--- Key mapping for searching files with Telescope
+-- Key mapping for searching files with Telescope.
 keymap('n', '<Space>sf', '<Cmd>Telescope find_files<CR>', opts)
-
--- Key mapping for searching words with Telescope
+-- Key mapping for searching words with Telescope.
 keymap('n', '<Space>sw', '<Cmd>Telescope live_grep<CR>', opts)
 
--- Key mapping for hovering LSP information with <Shift-k>
+-- Key mapping for hovering LSP information with <Shift-k>.
 keymap('n', '<S-k>', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
--- Key mapping for jumping to the next warning/error with <C-k>
+-- Key mapping for jumping to the next warning/error with <C-k>.
 keymap('n', '<C-k>', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 
--- Naviagate buffers
+-- Naviagate buffers.
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Toggle Nvim Tree
+-- Toggle Nvim Tree.
 keymap('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
 
--- Center the screen after scrolling with 'j' or 'k'
+-- Center the screen after scrolling with 'j' or 'k'.
 vim.api.nvim_set_keymap('n', 'j', 'jzz', { noremap = true })
 vim.api.nvim_set_keymap('n', 'k', 'kzz', { noremap = true })
