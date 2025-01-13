07f7e05f21e9ced8dd3d62d135c7a0e683b9c3db
Felipe Péret <sasdelli.pi@gmail.com>
2025-01-12 23:23:09 -0300

    Including gamemode

--- nixOS/packages/games.nix
+++ nixOS/packages/games.nix
@@ -3,10 +3,10 @@
     ############################################################################
     # Games
     # --------------------------------------------------------------------------
+    gamemode # Optimise Linux system performance on demand.
     ttyper # A terminal typing game.
     dsda-doom # An Doom source port with a focus on speedrunning.
     gzdoom # Modder-friendly source port based on the DOOM engine.
-    pokemmo-installer # Installer and Launcher for the PokeMMO emulator.
     ############################################################################
   ];
 }
