fa6d1e662d85b91ef7cc7b775b961ef1d9d2a8dc
Felipe Péret <sasdelli.pi@gmail.com>
2025-01-15 17:08:56 -0300

    Space Cadet Pinball

--- nixOS/packages/games.nix
+++ nixOS/packages/games.nix
@@ -7,6 +7,7 @@
     ttyper # A terminal typing game.
     dsda-doom # An Doom source port with a focus on speedrunning.
     gzdoom # Modder-friendly source port based on the DOOM engine.
+    space-cadet-pinball # Reverse engineering of 3D Pinball for Windows.
     ############################################################################
   ];
 }
