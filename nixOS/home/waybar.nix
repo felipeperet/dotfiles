ec77cbd6e26d98725fe2ad8e79a1c1604d31f0e3
Felipe Péret <sasdelli.pi@gmail.com>
2025-01-17 09:59:48 -0300

    Changing interval for battery on Waybar

--- nixOS/home/waybar.nix
+++ nixOS/home/waybar.nix
@@ -61,6 +61,7 @@
               warning = 30;
               critical = 15;
             };
+            interval = 60;
             format = "{capacity}% {icon}";
             format-charging = "{capacity}% 󰂄";
             format-plugged = "{capacity}% ";
