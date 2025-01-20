45cbcef627f01862fb1a12d10dac2d6944491b70
Felipe Péret <sasdelli.pi@gmail.com>
2025-01-19 23:45:50 -0300

    Enabling Python and testing libunity

--- nixOS/packages/games.nix
+++ nixOS/packages/games.nix
@@ -3,6 +3,7 @@
     ############################################################################
     # Games
     # --------------------------------------------------------------------------
+    mangohud # Vulkan and OpenGL overlay for monitoring.
     ttyper # A terminal typing game.
     dsda-doom # An Doom source port with a focus on speedrunning.
     gzdoom # Modder-friendly source port based on the DOOM engine.
