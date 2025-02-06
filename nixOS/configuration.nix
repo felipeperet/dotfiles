50aaf494c9c02c7087f16488d86fab2d82c8d6c7
Felipe Péret <sasdelli.pi@gmail.com>
2025-02-06 00:17:57 -0300

    New machine

--- nixOS/configuration.nix
+++ nixOS/configuration.nix
@@ -144,6 +144,12 @@
   i18n = {
     defaultLocale = "en_GB.UTF-8";
 
+    supportedLocales = [
+      "en_GB.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "pt_BR.UTF-8/UTF-8"
+    ];
+
     extraLocaleSettings = {
       LC_ADDRESS = "pt_BR.UTF-8";
       LC_IDENTIFICATION = "pt_BR.UTF-8";
