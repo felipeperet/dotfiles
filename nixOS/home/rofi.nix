3970e938a8f4da36e297abfde0fe5c2abe1fb2d1
Felipe Péret <40478095+felipeperet@users.noreply.github.com>
2025-10-15 20:57:05 -0300

    Changing rofi settings

--- nixOS/home/rofi.nix
+++ nixOS/home/rofi.nix
@@ -12,22 +12,52 @@
       theme = let
         inherit (config.lib.formats.rasi) mkLiteral;
       in {
-        element-icon = {
-          size = mkLiteral "4ch";
+        window = {
+          background-color = lib.mkForce (mkLiteral "rgba(0, 0, 0, 0.4)");
+          width = lib.mkForce (mkLiteral "50%");
+          location = lib.mkForce (mkLiteral "center");
+          anchor = lib.mkForce (mkLiteral "center");
+          border-radius = lib.mkForce (mkLiteral "20px");
         };
 
-        window = {
-          border-radius = mkLiteral "15px";
+        mainbox = {
+          background-color = lib.mkForce (mkLiteral "rgba(20, 20, 20, 0.9)");
+          padding = lib.mkForce (mkLiteral "40px");
         };
 
         inputbar = {
-          children = [(mkLiteral "entry")];
+          children = lib.mkForce [(mkLiteral "entry")];
+          background-color = lib.mkForce (mkLiteral "rgba(255, 255, 255, 0.1)");
+          padding = lib.mkForce (mkLiteral "20px");
+          border-radius = lib.mkForce (mkLiteral "15px");
+          margin = lib.mkForce (mkLiteral "0 0 20px 0");
         };
 
         entry = {
-          padding = mkLiteral "6px";
-          margin = mkLiteral "20px 0px 0px 20px";
-          border-radius = mkLiteral "15px";
+          padding = lib.mkForce (mkLiteral "10px");
+          background-color = lib.mkForce (mkLiteral "transparent");
+          text-color = lib.mkForce (mkLiteral "rgba(255, 255, 255, 1.0)");
+        };
+
+        listview = {
+          background-color = lib.mkForce (mkLiteral "transparent");
+          spacing = lib.mkForce (mkLiteral "10px");
+          lines = lib.mkForce 6;
+        };
+
+        element = {
+          padding = lib.mkForce (mkLiteral "15px");
+          border-radius = lib.mkForce (mkLiteral "10px");
+          background-color = lib.mkForce (mkLiteral "transparent");
+        };
+
+        element-icon = {
+          size = lib.mkForce (mkLiteral "3em");
+          margin = lib.mkForce (mkLiteral "0 15px 0 0");
+        };
+
+        element-text = {
+          text-color = lib.mkForce (mkLiteral "rgba(255, 255, 255, 0.9)");
         };
       };
     };
