d3c3d2aa797765072a3586bc9db5cfa5f4c1ef64
Felipe Péret <40478095+felipeperet@users.noreply.github.com>
2026-01-10 19:35:24 -0300

    Adding nixpkgs

--- nixOS/packages/games.nix
+++ nixOS/packages/games.nix
@@ -5,6 +5,7 @@
     # --------------------------------------------------------------------------
     ttyper # A terminal typing game.
     space-cadet-pinball # Reverse engineering of 3D Pinball for Windows.
+    lutris # Open Source gaming platform for GNU/Linux.
     ############################################################################
   ];
 }
