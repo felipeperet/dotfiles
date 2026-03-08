fa38615ad777084676311dd101894f63955d6325
Felipe Péret <40478095+felipeperet@users.noreply.github.com>
2026-03-08 19:41:24 -0300

    Improving dunst

--- nixOS/home/dunst.nix
+++ nixOS/home/dunst.nix
@@ -11,19 +11,40 @@
       settings = {
         global = {
           # Geometry
-          width = 280;
+          width = 320;
+          height = 150;
+          padding = 14;
+          horizontal_padding = 16;
+          text_icon_padding = 12;
+          gap_size = 8;
           origin = "top-right";
           offset = "30x30";
 
           # Appearance
-          corner_radius = 10;
+          corner_radius = 14;
           frame_width = 2;
-          gap_size = 5;
+          font = "${config.stylix.fonts.sansSerif.name} 13";
 
           # Icons
           icon_position = "left";
           min_icon_size = 32;
-          max_icon_size = 64;
+          max_icon_size = 48;
+        };
+
+        urgency_low = {
+          background = "#1e1e2e99";
+          foreground = "#cdd6f4";
+          frame_color = "#313244";
+        };
+        urgency_normal = {
+          background = "#1e1e2e99";
+          foreground = "#cdd6f4";
+          frame_color = "#313244";
+        };
+        urgency_critical = {
+          background = "#1e1e2ecc";
+          foreground = "#f38ba8";
+          frame_color = "#f38ba8";
         };
       };
     };
